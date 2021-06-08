b691c2eaa94291b97d2a222d8978c31def51972d
Basel Atalla <55560502+baselatalla@users.noreply.github.com>
2021-06-08 14:20:24 +0300

    Update series.py

--- math_series/series.py
+++ math_series/series.py
@@ -1,5 +1,15 @@
 
 def fibonacci(n):
+     """
+    Recursive function that calculat thn number fibonacci
+
+    Arguments:
+        n:integer -- the number of sequence we are looking for
+        
+    Returns:
+        nth Number fo the lucas fibonacci
+       
+    """
     if n == 0:
         return 0
     if n == 1:
@@ -7,6 +17,16 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)  
 
 def lucas(n):
+    """
+    Recursive function that calculat thn number Lucas
+
+    Arguments:
+        n:integer -- the number of sequence we are looking for
+        
+    Returns:
+        nth Number fo the lucas series
+       
+    """
     if n == 0:
         return 2
     if n == 1:
@@ -14,6 +34,18 @@ def lucas(n):
     return lucas(n-1) + lucas(n-2)
 
 def sum_series( n , zero = 0, one = 1):
+    """
+    Recursive function that solve a sum series similar to Fibonacci and Lucas
+
+    Arguments:
+        n:integer -- the number of sequence we are looking for
+        a:optional integer -- value of series at index 0
+        b:optional integer -- value of series at index 1
+
+    Returns:
+        Number similar to Fibonacci and Lucas series 
+        but could be any other math series based on the first two values
+    """
     if n == 0:
         return zero
     if n == 1:
